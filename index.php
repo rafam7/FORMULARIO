49d4ef6eb1330ae31dd18fcff64dd4ca01c76141
Alextre88 <trejotnt@hotmail.com>
2016-11-26 13:42:55 -0600

    formulario

---
+++ index.php
@@ -0,0 +1,50 @@
+<HTML>
+<head>
+  <meta charset="UTF-8">
+  <title>Asesor del Descanso</title>
+
+
+ </head>
+
+<FORM ACTION=prog.php METHOD=post>
+1.-Edad.<br>
+<input type=radio name=EDAD value=Reciennacido a 18 años>Recien nacido a 18 años.<br>
+<input type=radio name=EDAD value=18 a 60 años>18 a 60 años.<br>
+<input type=radio name=EDAD value=60 años en adelante>60 años en adelante.<br><br>
+
+2.-Mi colchón es para descansar acompañado o sólo.<br>
+<input type=radio name=SOL value=Sólo>Solo.<br>
+<input type=radio name=SOL value=Acompañado>Acompañado.<br><br>
+
+3.-Presento algún dolor en columna, huesos, insomnio o alguna enfermedad que me incomode al dormir.<br>
+<input type=radio name=SI value=Si>Si.<br>
+<input type=radio name=SI value=No>No.<br><br>
+
+4.-Mi peso es de (Los colchones resisten un peso corporal de más de 300 kilos, pero es importante la firmeza para un buen descanso y si va proporcional al peso de cada persona).<br>
+<input type=radio name=PESO value=Menos de 50 kilos>Menos de 50 kilos.<br>
+<input type=radio name=PESO value=Entre 50 y 80 kilos>Entre 50 y 80 kilos.<br>
+<input type=radio name=PESO value=Entre 80 y 100 kilos>Entre 80 y 100 kilos.<br>
+<input type=radio name=PESO value=Más de 100>Más de 100.<br><br>
+
+5.-Me gusta dormir en un colchón.<br>
+<input type=radio name=GUSTA value=Firme>Firme<br>
+<input type=radio name=GUSTA value=Suave>Suave<br>
+<input type=radio name=GUSTA value=Medio>Medio<br><br>
+
+6.-Qué tecnología busco en mi colchón.<br>
+<input type=radio name=BUSCO value=Resortes convencionales>Resortes convencionales.<br>
+<input type=radio name=BUSCO value=Resortes templados (Firmes)>Resortes templados (Firmes).<br>
+<input type=radio name=BUSCO value=Resortes embolsados>Resortes embolsados.<br>
+<input type=radio name=BUSCO value=Aglutinado>Aglutinado.<br>
+<input type=radio name=BUSCO value=Memory Foam>Memory Foam.<br><br>
+
+7.-Tengo un presupuesto de.<br>
+<input type=radio name=TENGO value=Menos de $2,000>Menos de $2,000.<br>
+<input type=radio name=TENGO value=De $2,000 a $4,000>De $2,000 a $4,000.<br>
+<input type=radio name=TENGO value=De $4,000 a $8,000>De $4,000 a $8,000.<br>
+<input type=radio name=TENGO value=Arriba de $8,000>Arriba de $8,000.<br><br>
+
+
+
+<INPUT TYPE=submit NAME=OK VALUE="Enviar"><BR>
+</FORM></HTML>
---
+++ prog.php
@@ -0,0 +1,18 @@
+<?php
+
+$EDAD=$_POST['EDAD'];
+$SOL=$_POST['SOL'];
+$SI=$_POST['SI'];
+$PESO=$_POST['PESO'];
+$GUSTA=$_POST['GUSTA'];
+$BUSCO=$_POST['BUSCO'];
+$TENGO=$_POST['TENGO'];
+
+echo "<B>EDAD =</B>".$EDAD."<br>";
+echo "<B>Mi colchón es para descansar acompañado o sólo =</B>".$SOL."<br>";
+echo "<B>Presento algún dolor en columna, huesos, insomnio o alguna enfermedad que me incomode al dormir =</B>".$SI."<br>";
+echo "<B>Mi peso es de (Los colchones resisten un peso corporal de más de 300 kilos, pero es importante la firmeza para un buen descanso y si va proporcional al peso de cada persona) =</B>".$PESO."<br>";
+echo "<B>Me gusta dormir en un colchón =</B>".$GUSTA."<br>";
+echo "<B>Qué tecnología busco en mi colchón =</B>".$BUSCO."<br>";
+echo "<B>Tengo un presupuesto de =</B>".$TENGO."<br>";
+?>
